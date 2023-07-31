a1723a4351b9b41c871a0e664c4454c2dd356f7e
Seba <seba@local.pl>
2023-07-31 19:40:12 +0200

    add image for hobby blog

--- project2/webBlog/blog/models.py
+++ project2/webBlog/blog/models.py
@@ -5,6 +5,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=150)
     text = models.TextField()
     date = models.DateField()
+    image = models.ImageField(upload_to='images/', blank=True)
 
     def __str__(self):
         return f"{self.title} | {self.date}"
