d33a48ea9d021fc744a0d257492a7b67b9a9cafe
Seba <seba@local.pl>
2023-07-31 22:51:58 +0200

    update color front and background

--- project1/passwordGeneratorProject/passwordGenerator/settings.py
+++ project1/passwordGeneratorProject/passwordGenerator/settings.py
@@ -17,8 +17,7 @@ import os
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -126,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
