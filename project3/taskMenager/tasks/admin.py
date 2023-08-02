03061ece0ffa7206f67dabc77c3866e665b996fe
Seba <seba@local.pl>
2023-08-02 19:52:30 +0200

    ceated Task model in project3

--- project3/taskMenager/tasks/admin.py
+++ project3/taskMenager/tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
+
+class CreateDate(admin.ModelAdmin):
+    readonly_fields = ('createDate',)
+
 
 # Register your models here.
+admin.site.register(Task, CreateDate)
