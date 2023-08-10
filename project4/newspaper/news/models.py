5f83791ef864003deea80cd96b5cb667bbe8c554
Seba <seba@local.pl>
2023-08-10 18:45:09 +0200

    add AuthToken autorization and tokens to project

--- project4/newspaper/news/models.py
+++ project4/newspaper/news/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
 
 # Create your models here.
 class Article(models.Model):
@@ -11,3 +14,9 @@ class Article(models.Model):
     def __str__(self):
         return f"{self.title} | {self.owner}"
     
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if kwargs['created']:
+        Token.objects.create(user=kwargs['instance'])
+    # print("Receiver działa")
+    # print(kwargs)
